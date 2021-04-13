fbeca80535125c77ff0335f43d25037c76536b09
Ivan <izzyumkin@gmail.com>
2021-04-13 21:19:48 +0500

    Initialize commit

---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by Иван Изюмкин on 13.04.2021.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
