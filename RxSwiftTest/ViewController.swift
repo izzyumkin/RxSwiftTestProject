27c2c74dfbb190cf715aa0f9b595acf4de6bc10a
Ivan <izzyumkin@gmail.com>
2021-04-13 21:52:02 +0500

    Show basic operations with RxSwift

--- RxSwiftTest/ViewController.swift
+++ RxSwiftTest/ViewController.swift
@@ -11,12 +11,33 @@ import RxCocoa
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var firstMessageLabel: UILabel!
+    
+    private var messages = BehaviorRelay(value: ["Hello i am Ann"])
+    
+    private let bag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        messages.asObservable()
+            .debug()
+            .throttle(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+            .filter({ value in
+                value.count > 1
+            }).map({ value in
+                value.joined(separator: ",\n")
+            }).subscribe(onNext: { [weak self] value in
+                
+                self?.firstMessageLabel.text = value
+                
+            }).disposed(by: bag)
         
+        messages.accept(["How are you?", "What would you like to do today?", "Your subscription ends in a month!!!"])
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            self?.messages.accept(["How do you like your new job?"])
+        }
         
     }
 
